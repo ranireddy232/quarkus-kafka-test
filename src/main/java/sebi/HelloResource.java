083198cbf2982278ce12218cb79945b9c9fcb473
Pradeep <pradeep.pakeer@falcorp.co.za>
2024-03-24 20:21:24 +0200

    Updated Docker files

---
+++ src/main/java/sebi/HelloResource.java
@@ -0,0 +1,21 @@
+package sebi;
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class HelloResource {
+
+    @ConfigProperty(name="deployment")
+    String deployment;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Developer Sandbox for Red Hat OpenShift, this has been deployed with : " + deployment;
+    }
+}
